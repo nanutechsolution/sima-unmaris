e1e65c7bc46345e121e01704b74c089c9f122e9f
nanu <ranusate19@gmail.com>
2026-03-07 02:44:46 +0800

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,7 +23,7 @@ class DatabaseSeeder extends Seeder
         // ]);
         $this->call([
             RolePermissionSeeder::class,
-            AssetMasterSeeder::class,
+            // AssetMasterSeeder::class,
         ]);
 
     }
