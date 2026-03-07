b675002918fcead09797c35fd4b0684473992fa9
nanu <ranusate19@gmail.com>
2026-03-07 22:10:29 +0800

    Update CategorySeeder.php

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -1,72 +1,30 @@
 <?php
 
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Str;
-use App\Models\Category;
 
 class CategorySeeder extends Seeder
 {
     public function run(): void
     {
         $categories = [
-            [
-                'name' => 'Komputer & IT',
-                'prefix_code' => 'IT',
-                'description' => 'Laptop, PC, server, printer, jaringan',
-            ],
-            [
-                'name' => 'Peralatan Laboratorium',
-                'prefix_code' => 'LAB',
-                'description' => 'Alat praktikum dan penelitian laboratorium',
-            ],
-            [
-                'name' => 'Furniture',
-                'prefix_code' => 'FUR',
-                'description' => 'Meja, kursi, lemari, rak',
-            ],
-            [
-                'name' => 'Peralatan Elektronik',
-                'prefix_code' => 'ELK',
-                'description' => 'Proyektor, sound system, TV',
-            ],
-            [
-                'name' => 'Peralatan Kantor',
-                'prefix_code' => 'ATK',
-                'description' => 'Mesin fotocopy, scanner, alat kantor',
-            ],
-            [
-                'name' => 'Kendaraan',
-                'prefix_code' => 'KND',
-                'description' => 'Mobil operasional dan motor kampus',
-            ],
-            [
-                'name' => 'Peralatan Olahraga',
-                'prefix_code' => 'OLR',
-                'description' => 'Peralatan olahraga kampus',
-            ],
-            [
-                'name' => 'Peralatan Kebersihan',
-                'prefix_code' => 'KBR',
-                'description' => 'Vacuum, alat pel, mesin kebersihan',
-            ],
-            [
-                'name' => 'Peralatan Keamanan',
-                'prefix_code' => 'SEC',
-                'description' => 'CCTV, alarm, alat keamanan',
-            ],
-            [
-                'name' => 'Peralatan Multimedia',
-                'prefix_code' => 'MED',
-                'description' => 'Kamera, lighting, alat produksi media',
-            ],
+            ['name' => 'Komputer & IT', 'prefix_code' => 'IT'],
+            ['name' => 'Peralatan Laboratorium', 'prefix_code' => 'LAB'],
+            ['name' => 'Furniture', 'prefix_code' => 'FUR'],
+            ['name' => 'Peralatan Elektronik', 'prefix_code' => 'ELK'],
+            ['name' => 'Peralatan Kantor', 'prefix_code' => 'ATK'],
+            ['name' => 'Kendaraan', 'prefix_code' => 'KND'],
+            ['name' => 'Peralatan Multimedia', 'prefix_code' => 'MED'],
+            ['name' => 'Peralatan Keamanan', 'prefix_code' => 'SEC'],
         ];
 
         foreach ($categories as $category) {
-            Category::create([
-                'id' => Str::uuid(),
+            \App\Models\Category::create([
+                'id' => \Illuminate\Support\Str::uuid(),
                 'name' => $category['name'],
                 'prefix_code' => $category['prefix_code'],
-                'description' => $category['description'],
             ]);
         }
     }
